ea6aeaf7d6c76a52bc9ff5bfc5c651f354bbc696
JUANPABLOHERNANDEZGONZALEZ <106846794+JUANPABLOHERNANDEZGONZALEZ@users.noreply.github.com>
2024-02-18 20:22:10 -0600

    Numeros a letras.py

--- Numeros a letras.py
+++ Numeros a letras.py
@@ -1,5 +1,8 @@
 from num2words import num2words
 numero = input("Introduce un numero: ")
 numero = float(numero)
-letras = num2words(numero, lang='es')
-print("El numero en letras es:", letras)
+if(numero > 0 and numero <= 1000000000000000000):
+    letras = num2words(numero, lang='es')
+    print("El numero en letras es:", letras)
+else:
+    print("Numero no valido")
