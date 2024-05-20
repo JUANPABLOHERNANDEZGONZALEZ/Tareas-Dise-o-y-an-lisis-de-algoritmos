58d63c0d62dc86d9fe1904b659af6ce1beffc802
JUANPABLOHERNANDEZGONZALEZ <106846794+JUANPABLOHERNANDEZGONZALEZ@users.noreply.github.com>
2024-05-20 15:34:18 -0600

    Add files via upload

---
+++ Quicksort.py
@@ -0,0 +1,20 @@
+def quicksort(arreglo, low, high):
+    if low < high:
+        pi = partition(arreglo, low, high)
+        quicksort(arreglo, low, pi - 1)
+        quicksort(arreglo, pi + 1, high)
+
+def partition(arreglo, low, high):
+    pivot = arreglo[high]
+    i = low - 1
+    for j in range(low, high):
+        if arreglo[j] < pivot:
+            i += 1
+            arreglo[i], arreglo[j] = arreglo[j], arreglo[i]
+    arreglo[i + 1], arreglo[high] = arreglo[high], arreglo[i + 1]
+    return i + 1
+
+arreglo = [6, 3, 1, 12, 0]
+quicksort(arreglo, 0, len(arreglo) - 1)
+
+print(arreglo)
